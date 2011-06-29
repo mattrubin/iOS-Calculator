0916d6da050dc9ffc13646e5e2ec4e42d8e6687b
Matt Rubin <mattrubin@me.com>
2011-06-28 22:47:34 -0400

    Added CalculatorBrain class stub

---
+++ Calculator/CalculatorBrain.h
@@ -0,0 +1,13 @@
+//
+//  CalculatorBrain.h
+//  Calculator
+//
+//  Created by Matt Rubin on 6/28/11.
+//  Copyright 2011 Matt Rubin. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CalculatorBrain : NSObject
+
+@end
---
+++ Calculator/CalculatorBrain.m
@@ -0,0 +1,23 @@
+//
+//  CalculatorBrain.m
+//  Calculator
+//
+//  Created by Matt Rubin on 6/28/11.
+//  Copyright 2011 Matt Rubin. All rights reserved.
+//
+
+#import "CalculatorBrain.h"
+
+@implementation CalculatorBrain
+
+- (id)init
+{
+    self = [super init];
+    if (self) {
+        // Initialization code here.
+    }
+    
+    return self;
+}
+
+@end
